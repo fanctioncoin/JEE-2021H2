10f6777749bed2c8b5553f07593067213befc373
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-09-17 20:20:26 +0300

    commit on the printWords branch

--- TestProject/src/main/java/com/example/test/Runner.java
+++ TestProject/src/main/java/com/example/test/Runner.java
@@ -4,5 +4,10 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Did I get you right?");
         System.out.println("Say it again, please");
+        printWords("thanks");
     }
-}
+
+        public static void printWords (String word){
+            System.out.println(word);
+        }
+    }
