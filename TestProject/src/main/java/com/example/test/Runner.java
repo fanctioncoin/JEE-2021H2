c359fbe6b9f4b46151269844f815b5bea0b297f1
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-09-17 21:14:20 +0300

    commit №2 on the printSimple

--- TestProject/src/main/java/com/example/test/Runner.java
+++ TestProject/src/main/java/com/example/test/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Did I get you right?");
         System.out.println("Say it again, please");
-        printSimpler("Some word");
+        printSimpler("Some word and another word");
     }
     static void printSimpler(String str){
         for (int i =0; i<5; i++){
