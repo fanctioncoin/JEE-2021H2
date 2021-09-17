1f134e6e88bee02815b807b175f0c59db2973628
Старавойт Вячеслав Александрович <fanctioncoin@gmail.com>
2021-09-17 15:45:51 +0300

    stop, ask me!

--- TestProject/src/main/java/com/example/test/Runner.java
+++ TestProject/src/main/java/com/example/test/Runner.java
@@ -3,5 +3,6 @@ package com.example.test;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Did I get you right?");
+        System.out.println("Say it again, please");
     }
 }
