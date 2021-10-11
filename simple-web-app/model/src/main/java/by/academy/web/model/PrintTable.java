c6bc52fce47a1b144ce0008316506c92d616b1ed
Vyacheslav Staravoyt <85665912+fanctioncoin@users.noreply.github.com>
2021-10-11 18:43:16 +0300

    Delete simple-web-app directory

--- simple-web-app/model/src/main/java/by/academy/web/model/PrintTable.java
+++
@@ -1,5 +0,0 @@
-package by.academy.web.model;
-
-public interface PrintTable {
-    String getInfo();
-}
--- simple-web-app/model/src/main/java/by/academy/web/model/Role.java
+++
@@ -1,7 +0,0 @@
-package by.academy.web.model;
-
-public enum Role { ADMIN,COACH,USER
-
-}
-
-
